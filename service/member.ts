e5a713e86ed24115e69b210ea8c932bb3d3e0a18
Eeung <lej6670@gmail.com>
2026-05-17 19:32:54 +0900

    프로필 섹션파일 추가

--- service/member.ts
+++ service/member.ts
@@ -2,16 +2,26 @@ import { api } from "#/lib/api";
 import { type Location } from "./types"
 
 export const getMe = async () => {
-  const res = await api.get("member/me");
+  const res = await api.get("members/me");
   return res.data;
 };
 
-export const setMe = async (data: any) => {
-  const res = await api.get("member/me", data);
+export const patchMe = async (data: any) => {
+  const res = await api.patch("members/me", data);
   return res;
 };
 
 export const getLocations = async(): Promise<Location[]> => {
-  const res = await api.get("member/me/locations");
+  const res = await api.get("members/me/locations");
+  return res.data;
+};
+
+export const patchLocations = async (): Promise<Location[]> => {
+  const res = await api.patch("members/me/locations");
+  return res.data;
+};
+
+export const deleteLocations = async (): Promise<Location[]> => {
+  const res = await api.delete("members/me/locations");
   return res.data;
 };
